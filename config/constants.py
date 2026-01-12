1f9a77eabf522a3d241b001d922415a044a74181
Piero010405 <psarmientoayala@gmail.com>
2026-01-11 19:51:26 -0500

    feat(config)

---
+++ config/constants.py
@@ -0,0 +1,10 @@
+BASE_URL = "http://diccionariovirtualyine.culturacusco.gob.pe"
+LIST_ENDPOINT = "/palabra-yine-all"
+
+MAX_PAGE = 174
+ITEM_SELECTOR = "div.li-filas-data.col-md-12"
+EXAMPLE_SELECTOR = "div.views-field-field-ejemplos-palabra em.field-content"
+
+RAW_HTML_DIR = "data/raw/html"
+PROCESSED_DIR = "data/processed"
+LOG_DIR = "data/logs"
---
+++ config/settings.py
@@ -0,0 +1,7 @@
+REQUEST_HEADERS = {
+    "User-Agent": "YineDatasetScraper/1.0 (academic use)"
+}
+
+TIMEOUT = 15
+SLEEP_SECONDS = 0.7
+RETRIES = 3
